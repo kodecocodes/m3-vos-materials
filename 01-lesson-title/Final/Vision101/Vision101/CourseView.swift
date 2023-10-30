23309db5e890aef70822db829234fe214466f945
Tim Mitra <tim@it-guy.com>
2023-10-30 02:08:36 -0400

    step 6

--- 01-lesson-title/Final/Vision101/Vision101/CourseView.swift
+++ 01-lesson-title/Final/Vision101/Vision101/CourseView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2023 Kodeco Inc.
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -31,55 +31,20 @@
 /// THE SOFTWARE.
 
 import SwiftUI
-import RealityKit
-import RealityKitContent
 
-struct Course: Identifiable {
-  let name: String
-  let id = UUID()
-}
-
-struct ContentListView: View {
+struct CourseView: View {
   
-  @State private var selectedCourse: Course? = nil
-  
-  private var courses = [
-    Course(name: "Window App"),
-    Course(name: "Volume App"),
-    Course(name: "Immersive App"),
-    Course(name: "Ornaments"),
-    Course(name: "App Icon")
-  ]
+  let course: Course
   
   var body: some View {
-    TabView {
-      NavigationView {
-        List(courses) {
-          Text($0.name)
-        }
-      }
-      .tabItem {
-        Image(systemName: "window.awning.closed")
-        Text("Window")
-      }
-      Text("Volume Tab")
-        .font(.system(size: 30, weight: .bold, design: .rounded))
-        .foregroundColor(.orange)
-        .tabItem {
-          Image(systemName: "cube")
-          Text("Volume")
-        }
-      Text("Immersive Tab")
-        .font(.system(size: 30, weight: .bold, design: .rounded))
-        .foregroundColor(.orange)
-        .tabItem {
-          Image(systemName: "globe")
-          Text("Immersive")
-        }
-    }.padding()
+    VStack {
+      Text(course.name)
+        .font(.largeTitle)
+    }
+    .navigationTitle(course.name)
   }
 }
 
-#Preview(windowStyle: .automatic) {
-  ContentListView()
+#Preview {
+  CourseView(course: Course(name: "visionOS"))
 }
