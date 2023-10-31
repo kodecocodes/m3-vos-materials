9bcafbfa3f907f8eec0ffa53b1d581e94bb76f0b
Tim Mitra <tim@it-guy.com>
2023-10-31 01:13:53 -0400

    step 15

--- 02-Volume-View/Final/Vision101/Vision101/Views/VolumeView.swift
+++ 02-Volume-View/Final/Vision101/Vision101/Views/VolumeView.swift
@@ -35,15 +35,31 @@ import RealityKit
 import RealityKitContent
 
 struct VolumeView: View {
+  
+  @State var runAnimation = false
+  
   var body: some View {
-    //  12
-    RealityView { content in
-      // Add the initial RealityKit content
-      if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-        content.add(scene)
-        // 14
-        scene.availableAnimations.forEach { animation in
-          scene.playAnimation(animation.repeat(), transitionDuration: 3, startsPaused: false)
+    // 15
+    VStack {
+      Button("Start") {
+        runAnimation.toggle()
+      }
+      //  12
+      RealityView { content in
+        // Add the initial RealityKit content
+        if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
+          content.add(scene)
+          // 14
+        }
+      } update: { content in
+        if let scene = content.entities.first {
+          scene.availableAnimations.forEach { animation in
+            if runAnimation {
+              scene.playAnimation(animation.repeat(), transitionDuration: 3, startsPaused: false)
+            } else {
+              scene.stopAllAnimations()
+            }
+          }
         }
       }
     }
