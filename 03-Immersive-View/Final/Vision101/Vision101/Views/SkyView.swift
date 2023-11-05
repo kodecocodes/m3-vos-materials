44d63db642376338015c527101ee022ed4a9d148
Tim Mitra <tim@it-guy.com>
2023-11-05 01:36:43 -0500

    step 27

--- 03-Immersive-View/Final/Vision101/Vision101/Views/SkyView.swift
+++ 03-Immersive-View/Final/Vision101/Vision101/Views/SkyView.swift
@@ -32,44 +32,27 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
-struct ImmersiveView: View {
-  var body: some View {
-    
-    // 27
-    SkyView()
-    // 18
-    RealityView { content in
-      if let scene = try? await Entity(named: "ImmersiveScene", in: realityKitContentBundle) {
-        content.add(scene)
-      }
-    } update: { content in
-      if let scene = content.entities.first {
-        scene.availableAnimations.forEach { animation in
-          scene.playAnimation(animation.repeat(), transitionDuration: 3, startsPaused: false)
+struct SkyView: View {
+    var body: some View {
+      RealityView { content in
+        guard let resource = try? await TextureResource(named: "skyView") else {
+          return
         }
-        // step 26
-        scene.orientation = simd_quatf(angle: Float.pi/4, axis: [-0.5, 0.0, 0.0])
-        // step 25
-        let orbit = OrbitAnimation(duration: 20.0,
-                                   axis: SIMD3<Float>(x: 0.1, y: 1.0, z: 0.0),
-                                   startTransform: scene.transform,
-                                   spinClockwise: false,
-                                   orientToPath: true,
-                                   rotationCount: 1.0,
-                                   bindTarget: .transform,
-                                   repeatMode: .repeat)
+        var material = UnlitMaterial()
+        material.color = .init(texture: .init(resource))
         
-        if let animation = try? AnimationResource.generate(with: orbit) {
-          scene.playAnimation(animation)
-        }
+        let entity = Entity()
+        entity.components.set(ModelComponent(mesh: .generateSphere(radius: 1000), materials: [material]
+        ))
+        // point the texture inwards
+        entity.scale *= .init(x: -1, y: 1, z: 1)
+        
+        content.add(entity)
       }
-    }.offset(y: -2000) // step 22
-      .offset(z: -1500)
-  }
+    }
 }
 
 #Preview {
-    ImmersiveView()
+    SkyView()
 }
